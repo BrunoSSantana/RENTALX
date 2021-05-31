1aa98ff1c5d95e508075c6698b9ec49e8bd49b56
BrunoSSantana <brunoosouza15@gmail.com>
2021-05-31 20:41:27 -0300

    criando migrations

---
+++ src/database/migrations/1622503323530-CreateCategories.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategories1622503323530 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+          new Table({
+              name: "categories",
+              columns: [
+                  {
+                    name: "id",
+                    type: "uuid"
+                  }
+              ];
+          })
+      )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
