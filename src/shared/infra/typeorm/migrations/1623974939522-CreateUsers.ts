e1cadddf0d88cf1547d490bb01d01f55c9186e8a
BrunoSSantana <brunoosouza15@gmail.com>
2021-07-06 14:24:30 -0300

    Add primary key to id column of user table

--- src/shared/infra/typeorm/migrations/1623974939522-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1623974939522-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1623974939522 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
